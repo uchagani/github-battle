15b74585c1fb0323340e5e6abe0c0c86c34c6eed
uchagani <umair.chagani@gmail.com>
2018-09-11 21:21:30 -0400

    built out routes and the home and battle components

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,29 @@
 var React = require('react');
 var Popular = require('./popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./nav');
+var Home = require('./home');
+var Battle = require('./battle');
+var Switch = ReactRouter.Switch
 
 class App extends React.Component {
   render() {
     return(
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/battle' component={Battle}/>
+            <Route path='/popular' component={Popular}/>          
+            <Route render={ function() {
+              return <p>Not Found</p>
+            }}/>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
